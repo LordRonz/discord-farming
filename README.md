934e7cf0ba4d9ab84a21aa6d7b30ea8a40f6ce4f
LordRonz <aarontanhar2000@gmail.com>
2021-03-22 18:13:43 +0700

    narto

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # discord-farming
 
 ## Sucking MEE6's cock
+
+# Dependencies
+### Python, of course
+### pyautogui and its dependencies
--- main.py
+++ main.py
@@ -1,37 +1,38 @@
-import pyautogui
-import time
-from random import choice
+def main():
+    try:
+        import pyautogui
+        import time
+        from random import choice
+        import sys
 
-time.sleep(3)
-width, height = pyautogui.size()
-# posx, posy = 0.3140625 * width, 0.95 * height
-loc = pyautogui.locateOnScreen('ss.png', grayscale=True)
-posx, posy = pyautogui.center(loc)
-pyautogui.moveTo(posx, posy)
-pyautogui.click()
-s1 = '''
-In William Shakespeare's Hamlet, "to be, or not to be, that is the question." In the 21st century, "to code, or not to code, that is the challenge.
-All that glitters is not gold
-By the pricking of my thumbs, Something wicked this way comes. Open, locks, Whoever knocks!
-Now is the winter of our discontent.
-Good night, good night! parting is such sweet sorrow, That I shall say good night till it be morrow.
-Hell is empty and all the devils are here.
-A horse! a horse! my kingdom for a horse!
-The lady doth protest too much, methinks.
-These violent delights have violent ends...
-Brevity is the soul of wit.
-'''
-s2 = [
-    'EXPECTO PATRONUM',
-    'AVADA KEDAVRA',
-    'WINGARDIUM LEVIOSA',
-    'EXPELLIARMUS',
-    'SECTUMSEMPRA',
-    'EXPECTO PATRONUM',
-        ]
-for i in range(1000):
-    s = choice(s2)
-    #s = s1
-    pyautogui.write(s)
-    pyautogui.press('enter')
-    time.sleep(62)
+        with open('./shakespeare.txt') as f:
+            s1 = f.read().splitlines()
+    
+        with open('./potterspells.txt') as f:
+            s2 = f.read().splitlines()
+
+        with open('./uzumakibayu.txt') as f:
+            s3 = f.read().splitlines()
+
+        time.sleep(3)
+        if len(sys.argv) < 3 or len(sys.argv) > 2 and (sys.argv[2] == '1' or sys.argv[2].lower() == 'true'):
+            width, height = pyautogui.size()
+            # posx, posy = 0.3140625 * width, 0.95 * height
+            loc = pyautogui.locateOnScreen('ss.png', grayscale=True)
+            posx, posy = pyautogui.center(loc)
+            pyautogui.moveTo(posx, posy)
+            pyautogui.click()
+
+        chosenOne = s3
+
+        while True:
+            s = choice(s3)
+            #s = s1
+            pyautogui.write(s)
+            pyautogui.press('enter')
+            time.sleep(62)
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
