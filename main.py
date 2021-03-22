a522e6e9d3b9f7ba50ebeae5ec56e470e618bda5
LordRonz <aarontanhar2000@gmail.com>
2021-03-22 18:24:27 +0700

    fixes

--- main.py
+++ main.py
@@ -15,22 +15,23 @@ def main():
             s3 = f.read().splitlines()
 
         time.sleep(3)
-        if len(sys.argv) < 3 or len(sys.argv) > 2 and (sys.argv[2] == '1' or sys.argv[2].lower() == 'true'):
+        if len(sys.argv) < 2 or (len(sys.argv) > 1 and (sys.argv[1] == '1' or sys.argv[1].lower() == 'true')):
             width, height = pyautogui.size()
             # posx, posy = 0.3140625 * width, 0.95 * height
             loc = pyautogui.locateOnScreen('ss.png', grayscale=True)
-            posx, posy = pyautogui.center(loc)
-            pyautogui.moveTo(posx, posy)
-            pyautogui.click()
+            if loc:
+                posx, posy = pyautogui.center(loc)
+                pyautogui.moveTo(posx, posy)
+                pyautogui.click()
 
         chosenOne = s3
 
         while True:
             s = choice(s3)
-            #s = s1
             pyautogui.write(s)
             pyautogui.press('enter')
             time.sleep(62)
+
     except Exception as e:
         print(e)
 
